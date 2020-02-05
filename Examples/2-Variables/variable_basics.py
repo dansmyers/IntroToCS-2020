59b407af554460069c437e5fdc1bed070f85d48a
Dan S. Myers <dmyers@rollins.edu>
2020-02-05 12:57:29 -0500

    Create variable_basics.py

---
+++ Examples/2-Variables/variable_basics.py
@@ -0,0 +1,15 @@
+# Basic idea: a variable is a name associated with a value
+# Values in Python are basic data: numbers, strings
+
+# Mental model: a "box" with a name that holds some data
+# Refer to the data in the box using its name
+
+# First example: area of a rectangle
+
+# Create variables to describe the length and width of
+# the rectangle
+length = 3
+width = 5
+
+# Use a variable assignment to calculate the area
+area = length * width
