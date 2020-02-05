5cad764fa38917acf9d5ecae456d640679f849db
Dan S. Myers <dmyers@rollins.edu>
2020-02-05 12:56:49 -0500

    Create magic_computer.py

---
+++ Examples/2-Variables/magic_computer.py
@@ -0,0 +1,21 @@
+"""
+The Magic Computer: a Mad Lib
+
+CMS 195, Spring 2020
+"""
+
+# Prompt the user to enter all of the required words
+noun1 = input('Enter a noun: ')
+plural_noun1 = input('Enter a plural noun: ')
+verb1 = input('Enter a present tense verb: ')
+verb2 = input('Enter a present tense verb: ')
+part_of_body = input('Ener a plural part of the body: ')
+adj1 = input('Enter an adjective: ')
+plural_noun2 = input('Enter a plural noun: ')
+adj2 = input('Enter an adjective: ')
+
+# Print out the story with the user's words mixed in
+print('Today, every student has a computer small enough to fit into his ' + noun1 + '.')
+print('He can solve any math problem by simply pushing the computer\'s little '+ plural_noun1 + '.')
+
+# We finished the rest of the story in class...
