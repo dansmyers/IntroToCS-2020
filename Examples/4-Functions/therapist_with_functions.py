013cbc9b29aa6c2d9baae4efd4048002c046a057
Dan S. Myers <dmyers@rollins.edu>
2020-02-10 14:22:30 -0500

    Create therapist_with_functions.py

---
+++ Examples/4-Functions/therapist_with_functions.py
@@ -0,0 +1,30 @@
+"""
+A second version of the therapist program using functions
+
+CMS 195, Spring 2020
+"""
+
+### Main
+
+print('How are you feeling today?')
+print('1. Happy')
+print('2. Sad')
+print('3. Neutral')
+
+choice = int(input('Choose one option: '))
+
+# Print a message based on the user's choice
+#
+# We'll start with this version then alter it to use functions
+
+if choice == 1:
+    
+    # Print a positive message
+    
+elif choice == 2:
+    
+    # Print an encouraging message
+    
+else:
+    
+    # Print a neutral message
