a74a9fa8ab09a82e7d8bbee182d74c59823721eb
Dan S. Myers <dmyers@rollins.edu>
2020-02-10 14:17:45 -0500

    Create spam.py

---
+++ Examples/4-Functions/spam.py
@@ -0,0 +1,20 @@
+"""
+Using multiple functions and thinking about program flow
+
+CMS 195, Spring 2020
+"""
+
+def print_spam():
+    print('Spam')
+    
+
+def print_eggs():
+    print('Eggs')
+
+
+### Main
+print_spam()
+
+print_eggs()
+
+print_spam()
