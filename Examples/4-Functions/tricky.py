84bd6d870e226f1349a2c5e81d453fead58c9115
Dan S. Myers <dmyers@rollins.edu>
2020-02-19 12:40:00 -0500

    Create tricky.py

---
+++ Examples/4-Functions/tricky.py
@@ -0,0 +1,20 @@
+"""
+A tricky set of functions using the same variable name in multiple places. What does this code output?
+
+CMS 195, Spring 2020
+"""
+
+def baz(x) 
+    x = x * 2;
+    return x;    
+
+
+def foo(value):
+    x = baz(value + 1)
+    return x
+
+
+### Main
+x = 1
+x = foo(x)
+print(x)
