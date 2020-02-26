0403ada8ec5d5e01a9aa985ffe66ef3b7444c829
Dan S. Myers <dmyers@rollins.edu>
2020-02-26 14:00:07 -0500

    Create dvisible_by_7.py

---
+++ Examples/5-Loops/dvisible_by_7.py
@@ -0,0 +1,18 @@
+""
+A second common pattern: combine a loop with an if statement
+
+General idea: loop over a range and select only certain values to process
+
+CMS 195, Spring 2020
+"""
+
+# Print numbers less than 100 divisible by 7
+
+n = 1
+
+while n < 100:
+    
+    if n % 7 == 0:
+        print(n)
+    
+    n = n + 1
